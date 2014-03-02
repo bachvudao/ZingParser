ce987e464923d5ffa1cfc5abafb479dc97e1eb7e
OpenShift guest <bachvudao@gmail.com>
2014-03-01 22:59:42 -0500

    Media control key listener

--- js/main.js
+++ js/main.js
@@ -1,15 +1,75 @@
 function ZingReaderViewModel($scope) {
+  var isMediaPlaying = false;
   var regEx = /param.*xmlURL=(.*)&amp/g;
   var yql_url = 'https://query.yahooapis.com/v1/public/yql';
 
+  var myPlaylist = [];
+
+  var cssSelector = {
+    jPlayer: "#jquery_jplayer_1",
+    cssSelectorAncestor: "#jp_container_1"
+  };
+  var options = {
+    swfPath: "/",
+    supplied: "mp3",
+    loop: true,
+    playlistOptions: {
+      autoPlay: true,
+      loopOnPrevious: true,
+      shuffleOnLoop: true,
+      enableRemoveControls: false,
+      displayTime: 'slow',
+      addTime: 'fast',
+      removeTime: 'fast',
+      shuffleTime: 'slow'
+    }
+  };
+
+  var myPlaylistControl = new jPlayerPlaylist(cssSelector, myPlaylist, options);
+
+  $("#jquery_jplayer_1").bind($.jPlayer.event.play, function () {
+    var current = myPlaylistControl.current,
+      playlist = myPlaylistControl.playlist;
+    jQuery.each(playlist, function (index, song) {
+      if (index == current) {
+        //$("#jp-current-song-text").html(obj.title);
+        var displayText = song.title + " by " + song.artist;
+        if (displayText.length > 21) {
+          displayText = displayText.substring(0, 21) + "...";
+        }
+
+        $("#jp-current-song-text").html(displayText);
+        isMediaPlaying = true;
+    }});
+  });
+  
+  $("#jquery_jplayer_1").bind($.jPlayer.event.pause, function () {
+    isMediaPlaying = false;
+  });
+
+  $scope.handleKeyStroke = function (ev) {
+    if (ev.which == 32) {
+      // spacebar
+      if(isMediaPlaying){
+        myPlaylistControl.pause();  
+      }else{
+        myPlaylistControl.play();
+      }
+    }else if(ev.which == 106) { //j
+      myPlaylistControl.previous()();
+    }else if(ev.which == 107) { //k
+      myPlaylistControl.next();
+    }
+  }
+
   $scope.submitUrl = function () {
-    
+
     // sometimes if the url does not contain the st song, the html will not be in the rigt form. Add it regardless
     var amendedZingUrl = $scope.zingUrl;
-    if(amendedZingUrl.indexOf("zing") != -1 && amendedZingUrl.indexOf("?st") == -1){
+    if (amendedZingUrl.indexOf("zing") != -1 && amendedZingUrl.indexOf("?st") == -1) {
       amendedZingUrl = amendedZingUrl + "?st=1";
     }
-    
+
     $.ajax({
       'url': yql_url,
       'data': {
@@ -34,7 +94,6 @@ function ZingReaderViewModel($scope) {
           }
         }
 
-
         // retrieve the song list
         $.ajax({
           'url': yql_url,
@@ -49,46 +108,6 @@ function ZingReaderViewModel($scope) {
               return;
             }
 
-            var myPlaylist = [];
-
-            var cssSelector = {
-              jPlayer: "#jquery_jplayer_1",
-              cssSelectorAncestor: "#jp_container_1"
-            };
-            var options = {
-              swfPath: "/",
-              supplied: "mp3",
-              loop: true,
-              playlistOptions: {
-                autoPlay: true,
-                loopOnPrevious: false,
-                shuffleOnLoop: true,
-                enableRemoveControls: false,
-                displayTime: 'slow',
-                addTime: 'fast',
-                removeTime: 'fast',
-                shuffleTime: 'slow'
-              }
-            };
-
-            var myPlaylistControl = new jPlayerPlaylist(cssSelector, myPlaylist, options);
-
-            $("#jquery_jplayer_1").bind($.jPlayer.event.play, function () {
-              var current = myPlaylistControl.current,
-                playlist = myPlaylistControl.playlist;
-              jQuery.each(playlist, function (index, song) {
-                if (index == current) {
-                  //$("#jp-current-song-text").html(obj.title);
-                  var displayText = song.title + " by " + song.artist;
-                  if(displayText.length > 21){
-                    displayText = displayText.substring(0,21) + "...";
-                  }
-                  
-                  $("#jp-current-song-text").html(displayText);
-                } // if condition end
-              });
-            });
-
             var data = response.query.results.item;
             for (var i = 0; i < data.length; i++) {
               myPlaylistControl.add({
@@ -96,8 +115,8 @@ function ZingReaderViewModel($scope) {
                 mp3: data[i].source,
                 artist: data[i].performer,
               });
-              
-              myPlaylistControl.play(0);    
+
+              myPlaylistControl.play(0);
             }
 
           },
